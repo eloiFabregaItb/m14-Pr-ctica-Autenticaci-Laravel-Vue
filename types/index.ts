ed4fc260db2f35b31fb3318edb6747149cf8104a
eloifabregaitb <eloi.grech.7e6@itb.cat>
2024-03-12 19:38:44 +0100

    practica 6

--- types/index.ts
+++ types/index.ts
@@ -1,44 +1,43 @@
-export interface LoginPayload {
+export interface User {
     email: string;
-    password: string;
-  }
-  
-  export interface RegisterPayload {
+    email_verified_at?: Date;
+    id: number;
     name: string;
-    email: string;
-    password: string;
-    password_confirmation: string;
+    two_factor_confirmed_at?: Date;
+    two_factor_recovery_codes?: number;
+    two_factor_secret?: string;
+    updated_at: Date;
+    created_at: Date;
   }
-  
-  
-  export interface User {
-    id:number;
-    name:string;
+export interface LoginPayload{
     email:string;
-    email_verified_at:Date | null;
-    two_factor_secret:string | null;
-    two_factor_recovery_codes: string | null;
-    created_at: Date | null;
-    updated_at: Date | null;
-  }
-  
-
-export interface Link {
-    id:number;
-    created_at:Date;
-    updated_at:Date;
-    short_link:string;
-    full_link:string;
-    user_id:number;
-    views:number;
+    password:string;
 }
-
+export interface RegisterPayload {
+    name:string,
+    email:string,
+    password:string,
+    password_confirmation:string
+}
+export interface RawLink {
+  id: number;
+  short_link: string;
+  full_link: string;
+  views: number;
+  created_at: string;
+  updated_at: string;
+}
+export interface Link extends Omit<RawLink, "created_at" | "updated_at"> {
+  created_at: Date;
+  updated_at: Date;
+}
+  
 export interface ErrorResponse {
     message: string;
-    errors: Record<string, string[]>
-}
-
-export interface PaginatedResponse <T>{
+    errors: Record<string, string[]>;
+  }
+  
+export interface PaginatedResponse<T> {
     current_page: number;
     data: T[];
     first_page_url: string | null;
@@ -46,14 +45,14 @@ export interface PaginatedResponse <T>{
     last_page: number;
     last_page_url: string | null;
     links: {
-        url: string | null;
-        label: string;
-        active: boolean;
-    }[]
+      url: string | null;
+      label: string;
+      active: boolean;
+    }[];
     next_page_url: string | null;
     path: string;
     per_page: number;
     prev_page_url: string | null;
     to: number;
     total: number;
-}
+  }
